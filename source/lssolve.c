5b0dc2d8e96efe1e3e829454a21b60ade9e4106d
s153283 <simon.kok@live.dk>
2017-11-19 13:13:43 +0100

    starting on dgels_ but not finish

--- source/lssolve.c
+++ source/lssolve.c
@@ -8,8 +8,25 @@ int main(int argc, char * argv[]) {
     printf("./lssolve A.txt b.txt solution.txt\n");
     return -1;
   }
+
   matrix_t *A = read_matrix(argv[1]);
   vector_t *b = read_vector(argv[2]);
+
+  char trans[2] = "T";
+
+  dgels_(trans,            /* 'N' or 'T'             */
+      &(A->m),             /* rows in A              */
+      &(A->n),             /* cols in A              */
+      &(b->n),             /* cols in B              */
+      A->A,                /* array A                */
+      const int * lda,     /* leading dimension of A */
+      b->v,                /* array B                */
+      const int * ldb,     /* leading dimension of B */
+      double * work,       /* workspace array        */
+      int * lwork,         /* workspace size         */
+      int * info           /* status code            */
+  )
+
 
   printf("Matrix A:\n");
   print_matrix(A);
