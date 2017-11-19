b8c506fbd48d06dd484a8c6c2a76b73ebc856e95
s153283 <simon.kok@live.dk>
2017-11-19 11:55:54 +0100

    lssolve.c is made so it takes filenames as input

--- source/lssolve.c
+++ source/lssolve.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include "matrix_io.h"
 #include "lssolve.h"
 
 int main(int argc, char * argv[]) {
+  if (argc != 4) {
+    printf("Invalid number of input. Correct input form:\n");
+    printf("./lssolve A.txt b.txt solution.txt\n");
+    return -1;
+  }
+  matrix_t *A = read_matrix(argv[1]);
+  vector_t *b = read_vector(argv[2]);
 
-  /* Write your code here */
+  printf("Matrix A:\n");
+  print_matrix(A);
+  printf("Vector b:\n");
+  print_vector(b);
+
+  printf("The solution will be saved at %s\n",argv[3]);
+
 
   return EXIT_SUCCESS;
 }
